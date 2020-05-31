07a3bbdc61040996b13c0628221ea72bfff22f30
Cerus <zocks1703@web.de>
2020-05-31 07:19:21 +0200

    Fix bug: users would not be removed from their previous company when joining another one

--- src/main/java/xyz/spedcord/server/endpoint/oauth/DiscordEndpoint.java
+++ src/main/java/xyz/spedcord/server/endpoint/oauth/DiscordEndpoint.java
@@ -1,15 +1,13 @@
 package xyz.spedcord.server.endpoint.oauth;
 
 import bell.oauth.discord.main.Response;
-import xyz.spedcord.server.endpoint.Endpoint;
 import io.javalin.http.Context;
-import org.eclipse.jetty.http.HttpStatus;
 import xyz.spedcord.server.company.Company;
 import xyz.spedcord.server.company.CompanyController;
+import xyz.spedcord.server.endpoint.Endpoint;
 import xyz.spedcord.server.joinlink.JoinLinkController;
-import xyz.spedcord.server.oauth.invite.InviteAuthResult;
 import xyz.spedcord.server.oauth.invite.InviteAuthController;
-import xyz.spedcord.server.response.Responses;
+import xyz.spedcord.server.oauth.invite.InviteAuthResult;
 import xyz.spedcord.server.user.User;
 import xyz.spedcord.server.user.UserController;
 
@@ -73,11 +71,20 @@ public class DiscordEndpoint extends Endpoint {
         }
         User user = userOptional.get();
 
-        if(company.getMemberDiscordIds().contains(user.getDiscordId())
+        if (company.getMemberDiscordIds().contains(user.getDiscordId())
                 || company.getOwnerDiscordId() == user.getDiscordId()) {
             //Responses.error(HttpStatus.FORBIDDEN_403, "You're already a member of this company").respondTo(context);
             context.redirect("https://www.spedcord.xyz/error/invite/3");
             return;
+        }
+
+        if (user.getCompanyId() != -1) {
+            optional = companyController.getCompany(user.getCompanyId());
+            if (optional.isPresent()) {
+                Company oldCompany = optional.get();
+                oldCompany.getMemberDiscordIds().remove(user.getDiscordId());
+                companyController.updateCompany(oldCompany);
+            }
         }
 
         user.setCompanyId(company.getId());
